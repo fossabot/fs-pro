2ecc7907b62b0b9cf2c3e910d7db7b2b95a0dc01
AliBasicCoder <aliahmedreda34@gmail.com>
2019-06-29 22:04:57 +0200

    Initial Commit

--- saveGit.js
+++ saveGit.js
@@ -3,6 +3,8 @@ const cp = require('child_process');
 cp.execSync('git add -A');
 console.log("----------- add the files to get ---------");
 cp.execSync('git commit -a -m \"Initial Commit\"');
-console.log("------------ commited the changes --------")
-cp.execSync('git push github master');
-console.log("------------- pushed to github ------------");
+console.log("------------ commited the changes --------");
+if (process.argv[2] !== '--no-push') {
+    cp.execSync('git push github master');
+    console.log("------------- pushed to github ------------");
+}
