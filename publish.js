3d35acef77906e0ffd8bc497195afcda62f46263
AliBasicCoder <aliahmedreda34@gmail.com>
2019-06-29 09:04:48 +0200

    Initial Commit

--- publish.js
+++ publish.js
@@ -12,6 +12,8 @@ var num1 = Number(match[1]);
 var num2 = Number(match[2]);
 var num3 = Number(match[3]);
 
+var prev = { num1, num2, num3 };
+
 num3++;
 if (num3 > 9) {
     num3 = 0;
@@ -32,15 +34,35 @@ console.log(num1, num2, num3);
 
 const cp = require('child_process');
 
-cp.execSync('npm test');
-console.log('------------ tested ----------------------');
-cp.execSync('tsc test --outdir out ');
-console.log("---------- compiled the files ------------");
-cp.execSync('git add -A');
-console.log("----------- add the files to get ---------");
-cp.execSync('git commit -a -m \"Initial Commit\"');
-console.log("------------ commited the changes --------")
-cp.execSync('git push github master');
-console.log("------------- pushed to github ------------");
-cp.execSync('npm publish');
-console.log("------------- succesfly published ----------");
+cp.exec('npm test', function (err, stdout, sterr) {
+    if (err) {
+        process.stdout.write(sterr);
+        console.log('------------- failed ------------------')
+        return
+    }
+    process.stdout.write(stdout);
+    try {
+        go();
+    } catch (err) {
+        console.log('------------- failed ------------------')
+
+        var newV = `${prev.num1}.${prev.num2}.${prev.num3}`
+
+        json.version = newV;
+
+        file.write(JSON.stringify(json));
+    }
+});
+function go() {
+    console.log('------------ tested ----------------------');
+    cp.execSync('tsc test --outdir out ');
+    console.log("---------- compiled the files ------------");
+    cp.execSync('git add -A');
+    console.log("----------- add the files to get ---------");
+    cp.execSync('git commit -a -m \"Initial Commit\"');
+    console.log("------------ commited the changes --------")
+    cp.execSync('git push github master');
+    console.log("------------- pushed to github ------------");
+    cp.execSync('npm publish');
+    console.log("------------- succesfly published ----------");
+}
