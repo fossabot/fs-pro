466b29ade524506c1a13c5b819e46535d23741e3
AliBasicCoder <aliahmedreda34@gmail.com>
2019-07-05 20:56:22 +0200

    ..

---
+++ async/File.ts
@@ -0,0 +1,24 @@
+import { File as SyncFile } from "../src/index";
+
+class File extends SyncFile {
+    constructor(name: string, trak: boolean = true) {
+        super(name, trak);
+        // @ts-ignore
+        const file = new SyncFile(name, false);
+        //@ts-ignore
+        for (const key in this.__proto__) {
+            //@ts-ignore
+            this.__proto__[key] = (p1, p2, p3) =>
+                new Promise((res, rej) => {
+                    try {
+                        const data = file[key](p1, p2, p3);
+                        res(data);
+                    } catch (err) {
+                        rej(err);
+                    }
+                })
+        }
+    }
+}
+
+export default File;
--- async/index.ts
+++ async/index.ts
@@ -1,26 +1,5 @@
-import { File as SyncFile } from "../src/index";
 import Dir from "./Dir";
-
-class File extends SyncFile {
-    constructor(name: string, trak: boolean = true) {
-        super(name, trak);
-        // @ts-ignore
-        const file = new SyncFile(name, false);
-        //@ts-ignore
-        for (const key in this.__proto__) {
-            //@ts-ignore
-            this.__proto__[key] = (p1, p2, p3) =>
-                new Promise((res, rej) => {
-                    try {
-                        const data = file[key](p1, p2, p3);
-                        res(data);
-                    } catch (err) {
-                        rej(err);
-                    }
-                })
-        }
-    }
-}
+import File from "./File";
 
 export { File, Dir }
 
